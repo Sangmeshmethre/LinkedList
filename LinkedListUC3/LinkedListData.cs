b1f7880763584e62c490a3ac0447e6ade20edba6
Sangmesh Methre <SangmeshMetre>
2022-11-05 01:39:29 +0530

    LinkedList

---
+++ LinkedListUC3/LinkedListData.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListUC3
+{
+    public class LinkedListData
+    {
+        //head is used as node start point
+        Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("Element added in linkedlist is : {0} ", +node.data);
+
+        }
+
+        //Append Method is also used to Add Elements at last
+
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("After Appending element to lastly added element  : {0}", data);
+
+
+        }
+
+        //Display Method is Used to Display elements
+
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
+            else
+            {
+                Console.WriteLine("Linked List contains :");
+
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.Next;
+                }
+            }
+
+        }
+    }
+}
---
+++ LinkedListUC3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUC3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Problems");
+
+            bool check = true;
+
+        Start:
+            while (check)
+            {
+                Console.WriteLine("Please Select options :\n" +
+                    "1)Elements Added in the Linked List using Add Method\n" +
+                    "2)Elements Appended in the linked List to Last using Append Method\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Adding Elements as follows :");
+                        LinkedListData list = new LinkedListData();
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Adding Elements as follows :");
+                        LinkedListData list1 = new LinkedListData();
+                        list1.Add(56);
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list1.Append(30);
+                        list1.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list1.Append(70);
+                        list1.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list1.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Please select proper option");
+                        break;
+
+                        goto Start;
+
+                }
+
+            }
+
+        }
+    }
+}
