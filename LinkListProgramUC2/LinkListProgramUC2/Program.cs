edb43e42641eb2c1df2a6f4c9c498805632aeddd
Sangmesh Methre <SangmeshMetre>
2022-11-05 01:33:14 +0530

    LinkedList

---
+++ LinkListProgramUC2/LinkListProgramUC2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkListProgramUC2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome To LinkList Oparation");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+        }
+    }
+}
