9af10dff7546c3719e01e1544c6baf85d9180c5d
Sangmesh Methre <SangmeshMetre>
2022-11-05 01:27:15 +0530

    LinkedList

---
+++ BasicProgarmLinkListUC1/BasicProgarmLinkListUC1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgarmLinkListUC1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To LinkList Oparation");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+        }
+    }
+}
