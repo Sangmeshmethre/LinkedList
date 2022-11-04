2270db76b833916ea520204ed076ceacca91a1c9
Sangmesh Methre <SangmeshMetre>
2022-11-05 02:17:24 +0530

    LinkedList

---
+++ LinkedListUC5/LinkedListData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUC5
+{
+    public class LinkedListData
+    {
+        //head is used as node start point
+        Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("Element added in linkedlist is : {0} ", +node.data);
+
+        }
+
+        //Append Method is also used to Add Elements at last
+
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("After Appending element to lastly added element  : {0}", data);
+        }
+
+
+        //Insert method takes two parameters to insert element in between the Node
+
+        public void Insert(int lastNode, Node NewData)
+        {
+            Node temp = head;
+
+            //while loop to check if our temp is null then it return head
+            //else it goes to another while loop
+
+            while (temp != null)
+            {
+                while (temp.data == lastNode)
+                {
+
+                    Node newnode = temp.Next;
+                    //store NewData to temp.Next
+
+                    temp.Next = NewData;
+
+                    temp.Next.Next = newnode;
+                    break;
+                }
+                temp = temp.Next;
+            }
+        }
+
+        //pop Method to delet elements from the linked list
+
+        public void pop()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("linked List is Empty");
+            }
+            else
+            {
+                Node popped = head;
+                head = head.Next;
+
+            }
+        }
+        //Display Method is Used to Display elements
+
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
+            else
+            {
+                Console.WriteLine("Linked List contains :");
+
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.Next;
+                }
+            }
+
+        }
+    }
+}
---
+++ LinkedListUC5/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUC5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Problems");
+
+            bool check = true;
+
+        Start:
+            while (check)
+            {
+                Console.WriteLine("Please Select options :\n" +
+                    "1)Elements Added in the Linked List using Add Method\n" +
+                    "2)Elements Appended in the linked List to Last using Append Method\n" +
+                    "3)Element Inserted At a Particular Position Using Insert \n" +
+                    "4)Element Deleted from the list using pop Method\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Adding Elements as follows :");
+                        LinkedListData list = new LinkedListData();
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Adding Elements as follows :");
+                        LinkedListData list1 = new LinkedListData();
+                        list1.Add(56);
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list1.Append(30);
+                        list1.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list1.Append(70);
+                        list1.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list1.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Adding Elements as follows :");
+                        LinkedListData list2 = new LinkedListData();
+                        list2.Add(56);
+                        list2.Add(70);
+                        //creating object of Node class and passing data 
+                        Node node = new Node(30);
+                        Console.WriteLine("Element inserted in between the list: {0}", node.data);
+                        Console.WriteLine("So After inserting the Final list appears as :");
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list2.Insert(56, node);
+                        list2.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Adding Elements as follows :");
+                        LinkedListData list3 = new LinkedListData();
+                        list3.Add(56);
+                        list3.Add(30);
+                        list3.Add(70);
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list3.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("First Element Popped from the list");
+                        list3.pop();
+                        list3.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        break;
+
+                    default:
+                        Console.WriteLine("Please select proper option");
+                        break;
+
+                        goto Start;
+
+                }
+
+            }
+
+        }
+    }
+
+}
